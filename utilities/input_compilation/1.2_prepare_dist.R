fa9a282b2daa310d26f3412dc904064068fd96fd
Thomas <thomaskeggin@hotmail.com>
2019-11-25 12:26:38 +0100

    Removed duplicate 1.2 script

--- utilities/input_compilation/1.2_prepare_dist.R
+++ utilities/input_compilation/1.2_prepare_dist.R
@@ -10,19 +10,18 @@
 ##################################################
 
 # load libraries ####
-lib <- c("raster","matrixStats","sp","gdistance","geosphere","parallel")
+lib <- c("raster","matrixStats","sp","gdistance","geosphere","parallel", "dplyr")
 sapply(lib,library, character.only=TRUE)
 
 # load data from previous step ####
 
 load("./temp.Rdata")
-geoTimes  <- seq(1,length(geoDepthList))
-inputData <- geoDepthList
 
 # set variables ####
 
 OutputDir   <- paste0("../../Input/WorldMap200-0Ma_multiple_6d")
-crossing_NA <- 0 # Set to 0 (conductance) making land impassible. See gdistance package documentation.
+crossing_NA <- 0     # Set to 0 (conductance) making land impassible. See gdistance package documentation.
+depth_cut   <- -1000 # set the depth cut-off
 
 # check, or create, output directories ####
 
@@ -40,40 +39,49 @@ if (!file.exists(file.path(OutputDir, "/plot"))) {
 
 # create distance matrices ####
 
-t_start <- length(inputData)  # the starting raster index
-t_end   <- 1                  # the final raster index (present day)
+t_start <- length(geoDepthList)  # the starting raster index
+t_end   <- 1                     # the final raster index (present day)
 
 for (i in t_start:t_end){
   
-  Step_1        <- geoDepthList[[i]]
-  formattedyear <- geoTimes[i]
+  raster_i <- geoDepthList[[i]]
+  crs(raster_i) <- "+proj=longlat +datum=WGS84"
+  age      <- geoTimes[i]
   
   #plot
-  jpeg(file.path(OutputDir, "plot", paste0(formattedyear,".jpg") ), width = 680, height = 480)
+  jpeg(file.path(OutputDir, "plot", paste0(round(age, digits = 2),".jpg") ), width = 680, height = 480)
   par(mar=c(0,0,0.2,0.5)+0.2, oma=c(0,0,0,0))
-  plot(Step_1, legend.width=1,  legend.shrink=0.64, axes=FALSE, box=FALSE, xlab="", ylab="")
-  title(paste("GaSM world @", formattedyear), line=-2.5, cex.main=3)
+  plot(raster_i, legend.width=1,  legend.shrink=0.64, axes=FALSE, box=FALSE, xlab="", ylab="")
+  title(paste("GaSM world @", round(age, digits = 2)), line=-2.5, cex.main=3)
   dev.off()
   
-  hab_180                  <- Step_1        # this is setting up the conductance (cost of dispersal) values for each cell in the raster
-  hab_180[is.na(hab_180)]  <- crossing_NA   # this gives the NA valued cells a cost for crossing (land)
-  hab_180[!is.na(hab_180)] <- 1             # this gives habitable cells a cost for crossing (1 = no change in cost)
+  conductObj                     <- raster_i      # this is setting up the conductance (cost of dispersal) values for each cell in the raster
+  conductObj[!is.na(conductObj)] <- 1             # this gives habitable cells a cost for crossing (1 = no change in cost)
+  conductObj[is.na(conductObj)]  <- crossing_NA   # this gives the NA valued cells a cost for crossing (land)
   
-  # create a transition object (conductance)
-  trans_hab_180 <- transition(hab_180, transitionFunction=min, directions=8) # create matrix with least cost value between each pair of cells (symmetrical?)
-  trans_hab_180 <- geoCorrection(trans_hab_180, type = "c", scl = T)         # correct for map distortion
-  #go around the world
-  points_180 <- rasterToPoints(Step_1)
-  points_180_habitable <- points_180[, 1:2] # filter here for habitable cells
+  # create a transition object (based on conductance)
+  transObj <- transition(conductObj, transitionFunction=min, directions=8) # create matrix with least cost value between each pair of cells (symmetrical?)
+  transObj <- geoCorrection(transObj, type = "r", scl = T)         # correct for map distortion
+  # filter by out cells by depth cut off
+  df_i            <- as.data.frame(raster_i, xy=TRUE, na.rm = TRUE) # this will remove NA cells
+  colnames(df_i)  <- c("x","y","depth")
+  df_i_habitable  <- filter(df_i, depth > depth_cut)[, 1:2]
+  mat_i_habitable <- data.matrix(df_i_habitable)
   
-  # calculate the least-cost distance between points.
-  geo_dist_m_ti <- costDistance(trans_hab_180,
-                                points_180_habitable,
-                                points_180_habitable)
+  # calculate the least-cost distance between points using the transition object and target (habitable) cells
+  geo_dist_m_ti <- costDistance(transObj,
+                                mat_i_habitable,
+                                mat_i_habitable)
   
   save(geo_dist_m_ti,file=file.path(OutputDir,"geo_dist_m", "geo_dist_m_ti" , paste0("geo_dist_m_ti_t_",i,".RData",sep="")) )
   
-  cat("Done with", formattedyear, "\n")
+  # filter out all_geo_hab raster cells by the depth cut off
+  # depth
+  values(geoDepthList[[i]])[values(geoDepthList[[i]]) <= depth_cut] <- NA
+  # temp
+  geoTempList[[1]][is.na(geoDepthList[[i]][])] <- NA
+  
+  cat("Done with", age, "\n")
   
 }
 
